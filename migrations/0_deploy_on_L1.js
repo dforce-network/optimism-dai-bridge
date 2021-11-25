7c97cc3b4a42eec6384fa5b69bf2ced74624c823
skyge <1506186404li@gmail.com>
2021-11-25 10:38:30 +0800

    :rocket: Add: Add scripts to deploy contracts.

---
+++ migrations/0_deploy_on_L1.js
@@ -0,0 +1,199 @@
+
+// Right click on the script name and hit "Run" to execute
+async function main() {
+  try {
+    console.log("Running deployWithEthers script...");
+    let tx;
+
+    // // Mainnet
+    // const l1USXContractAddress = "0x0a5e677a6a24b2f1a2bf4f3bffc443231d2fdec8";
+    // const l2USXContractAddress = "0x641441c631e2F909700d2f41FD87F0aA6A6b4EDb";
+    // const l1MessengerAddress = "0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef";
+    // const inboxAddress = "0x4dbd4fc535ac27206064b68ffcf827b0a60bab3f";
+    // let l2USXGatewayProxyAddress = "";
+    // let proxyAdminAddress = "";
+    // let escrowImplContractAddress = "";
+    // let escrowProxyContractAddress = "";
+    // let l1USXGatewayImplAddress = "";
+    // let l1USXGatewayProxyAddress = "";
+    // let l2GovernanceRelayAddress = "";
+    // let l1GovernanceRelayImplAddress = "";
+    // let l1GovernanceRelayProxyAddress = "";
+    // Kovan
+    const l1USXContractAddress = "0xF76eAd4da04BbeB97d29F83e2Ec3a621d0FB3c6e";
+    const l2USXContractAddress = "0xab7020476D814C52629ff2e4cebC7A8cdC04F18E";
+    const l1MessengerAddress = "0x4361d0F75A0186C05f971c566dC6bEa5957483fD";
+    // const inboxAddress = "0x4dbd4fc535ac27206064b68ffcf827b0a60bab3f";
+    let l2USXGatewayProxyAddress = "0xB4d37826b14Cd3CB7257A2A5094507d701fe715f";
+
+    let proxyAdminAddress = "0xca64293ed5C3F3CdF110184d6E8fDDBfb0fa1728";
+    let escrowImplContractAddress = "0x9903Cd2f3d21ff9C85e12590c802B4F852dD44f8";
+    let escrowProxyContractAddress = "0x024Fa49a548e3496A0468b0f71cD3A4514B5D192";
+    let l1USXGatewayImplAddress = "0xae71BB4a2A10D1a252A057c316225b0BC22e2392";
+    let l1USXGatewayProxyAddress = "0x40E862341b2416345F02c41Ac70df08525150dC7";
+    let l2GovernanceRelayAddress = "0x57f76F87CE81638561070389f16D7833080c11a8";
+    let l1GovernanceRelayImplAddress = "0xfa901Da254c0964987B8e7FF814dCF01e9Ed93f7";
+    let l1GovernanceRelayProxyAddress = "0xE5ecB56521B4BF6E38662B2BD85143F5c56cAE90";
+
+    // 'web3Provider' is a remix global variable object
+    const signer = new ethers.providers.Web3Provider(web3Provider).getSigner();
+    let from = await signer.getAddress();
+
+    // 0. Deploys proxy admin.
+    const proxyAdminName = "ProxyAdmin";
+    const proxyAdminArtifactsPath = `browser/artifacts/contracts/library/${proxyAdminName}.sol/${proxyAdminName}.json`;
+    const proxyAdminMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", proxyAdminArtifactsPath)
+    );
+
+    if (!proxyAdminAddress) {
+      console.log("U r going to deploy proxy admin");
+      // Create an instance of a Contract Factory
+      const proxyAdminFactory = new ethers.ContractFactory(proxyAdminMetadata.abi, proxyAdminMetadata.bytecode, signer);
+      const proxyAdmin = await proxyAdminFactory.deploy();
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await proxyAdmin.deployed();
+      proxyAdminAddress = proxyAdmin.address;
+    }
+    console.log("proxy admin contract address: ", proxyAdminAddress);
+
+    // 1.0 Deploys escrow implementation contract
+    const escrowContractName = "L1Escrow";
+    const escrowPath = `browser/artifacts/contracts/l1/${escrowContractName}.sol/${escrowContractName}.json`;
+    const escrowMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", escrowPath)
+    );
+    if (!escrowImplContractAddress) {
+      console.log("U r going to deploy escrow implementation contract!");
+      // Create an instance of a Contract Factory
+      const escrowImplFactory = new ethers.ContractFactory(escrowMetadata.abi, escrowMetadata.bytecode, signer);
+      const escrowImpl = await escrowImplFactory.deploy();
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await escrowImpl.deployed();
+      escrowImplContractAddress = escrowImpl.address;
+    }
+    console.log("Escrow implementation contract address: ", escrowImplContractAddress);
+    const escrowInIface = new ethers.utils.Interface(escrowMetadata.abi);
+
+    // 1.1 Deploys escrow proxy contract
+    const proxyName = "TransparentUpgradeableProxy";
+    const proxyArtifactsPath = `browser/artifacts/@openzeppelin/contracts/proxy/${proxyName}.sol/${proxyName}.json`;
+    const proxyMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", proxyArtifactsPath)
+    );
+
+    if (!escrowProxyContractAddress) {
+      console.log("Going to deploy escrow proxy contract!");
+      const escrowInitData = escrowInIface.encodeFunctionData("initialize", []);
+      console.log("initData is: ", escrowInitData);
+
+      const escrowProxyFactory = new ethers.ContractFactory(proxyMetadata.abi, proxyMetadata.bytecode, signer);
+      const escrowProxy = await escrowProxyFactory.deploy(escrowImplContractAddress, proxyAdminAddress, escrowInitData);
+      await escrowProxy.deployed();
+      escrowProxyContractAddress = escrowProxy.address;
+    }
+    console.log("Escrow proxy contract address: ", escrowProxyContractAddress);
+    let escrow = new ethers.Contract(escrowProxyContractAddress, escrowMetadata.abi, signer);
+
+
+    // 2. Get current nonce to calculate contract address of next deployed contract.
+    // cause will deploy implementation and proxy contract.
+    let nonce = await signer.getTransactionCount() + 1;
+    console.log("Deployer nonce is: ", nonce);
+    let addressOfNextDeployedContract = ethers.utils.getContractAddress({ from, nonce });
+    console.log("Next deploy contract L1 governance relay address is: ", addressOfNextDeployedContract);
+
+    nonce = await signer.getTransactionCount() + 3;
+    console.log("Deployer nonce is: ", nonce);
+    addressOfNextDeployedContract = ethers.utils.getContractAddress({ from, nonce });
+    console.log("Next deploy contract L1 USX gateway address is: ", addressOfNextDeployedContract);
+
+    console.log("\nRun another script to deploy contract on the L2\n");
+    // return;
+
+    // 3.0 Deploy L1 governance rely implementation contract.
+    const governanceRelayContractName = "L1GovernanceRelay";
+    const governanceRelayPath = `browser/artifacts/contracts/l1/${governanceRelayContractName}.sol/${governanceRelayContractName}.json`;
+    const governanceRelayMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", governanceRelayPath)
+    );
+
+    let l1governanceRelayInitArgs = [
+      l2GovernanceRelayAddress,
+      l1MessengerAddress,
+    ];
+    if (!l1GovernanceRelayImplAddress) {
+      console.log("U r going to deploy governance relay implementation contract!");
+      // Create an instance of a Contract Factory
+      const governanceRelayImplFactory = new ethers.ContractFactory(governanceRelayMetadata.abi, governanceRelayMetadata.bytecode, signer);
+      const governanceRelayImpl = await governanceRelayImplFactory.deploy(...l1governanceRelayInitArgs);
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await governanceRelayImpl.deployed();
+      l1GovernanceRelayImplAddress = governanceRelayImpl.address;
+    }
+    console.log("Governance relay implementation contract address: ", l1GovernanceRelayImplAddress);
+    const governanceRelayInIface = new ethers.utils.Interface(governanceRelayMetadata.abi);
+
+    // 3.1 Deploys L1 governance rely proxy contract.
+    if (!l1GovernanceRelayProxyAddress) {
+      console.log("Going to deploy governance relay proxy contract!");
+      const governanceRelayInitData = governanceRelayInIface.encodeFunctionData("initialize", [...l1governanceRelayInitArgs]);
+      console.log("initData is: ", governanceRelayInitData);
+
+      const governanceRelayProxyFactory = new ethers.ContractFactory(proxyMetadata.abi, proxyMetadata.bytecode, signer);
+      const governanceRelayProxy = await governanceRelayProxyFactory.deploy(l1GovernanceRelayImplAddress, proxyAdminAddress, governanceRelayInitData);
+      await governanceRelayProxy.deployed();
+      l1GovernanceRelayProxyAddress = governanceRelayProxy.address;
+    }
+    console.log("L1 governance realy proxy contract address: ", l1GovernanceRelayProxyAddress);
+
+    // 4.0 Deploys L1 USX gateway implementation contract
+    let l1USXGatewayInitArgs = [
+      l1USXContractAddress,
+      l2USXGatewayProxyAddress,
+      l2USXContractAddress,
+      l1MessengerAddress,
+      escrowProxyContractAddress
+    ];
+    const l1USXGatewayContractName = "L1USXTokenBridge";
+    const l1USXGatewayPath = `browser/artifacts/contracts/l1/${l1USXGatewayContractName}.sol/${l1USXGatewayContractName}.json`;
+    const l1USXGatewayMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", l1USXGatewayPath)
+    );
+    if (!l1USXGatewayImplAddress) {
+      console.log("U r going to deploy L1 USX gateway implementation contract!");
+      // Create an instance of a Contract Factory
+      const l1USXGatewayImplFactory = new ethers.ContractFactory(l1USXGatewayMetadata.abi, l1USXGatewayMetadata.bytecode, signer);
+      const l1USXGatewayImpl = await l1USXGatewayImplFactory.deploy(...l1USXGatewayInitArgs);
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await l1USXGatewayImpl.deployed();
+      l1USXGatewayImplAddress = l1USXGatewayImpl.address;
+    }
+    console.log("L1 USX gateway implementation contract address: ", l1USXGatewayImplAddress);
+    const l1USXGatewayInIface = new ethers.utils.Interface(l1USXGatewayMetadata.abi);
+
+    // 4.1 Deploys L2 USX gateway proxy contract
+    if (!l1USXGatewayProxyAddress) {
+      console.log("Going to deploy L1 USX gateway proxy contract!");
+      const l1USXGatewayInitData = l1USXGatewayInIface.encodeFunctionData("initialize", [...l1USXGatewayInitArgs]);
+      console.log("l1 USX Gateway initData is: ", l1USXGatewayInitData);
+
+      const l1USXGatewayProxyFactory = new ethers.ContractFactory(proxyMetadata.abi, proxyMetadata.bytecode, signer);
+      const l1USXGatewayProxy = await l1USXGatewayProxyFactory.deploy(l1USXGatewayImplAddress, proxyAdminAddress, l1USXGatewayInitData);
+      await l1USXGatewayProxy.deployed();
+      l1USXGatewayProxyAddress = l1USXGatewayProxy.address;
+    }
+    console.log("L1 USX Gateway proxy contract address: ", l1USXGatewayProxyAddress);
+    return;
+
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+main()
+  .then(() => console.log('DONE'))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ migrations/1_deploy_on_L2.js
@@ -0,0 +1,151 @@
+// Right click on the script name and hit "Run" to execute
+async function main() {
+  try {
+    console.log("Running deployWithEthers script...");
+    let tx;
+
+    const deployerAddress = "";
+
+    // // Optimism
+    // const l1USXContractAddress = "0x0a5e677a6a24b2f1a2bf4f3bffc443231d2fdec8";
+    // const l2USXContractAddress = "0x641441c631e2F909700d2f41FD87F0aA6A6b4EDb";
+    // const l2msdControllerAddress = "0x38a5585d347E8DFc3965C1914498EAfbDeD7c5Ff";
+    // const l2RouterAddress = "0x5288c571Fd7aD117beA99bF60FE0846C4E84F933";
+    // let l1USXGatewayProxyAddress = "";
+    // let proxyAdminAddress = "";
+    // let l2USXGatewayImplAddress = "";
+    // let l2USXGatewayProxyAddress = "";
+    // let l1GovernanceRelayAddress = "";
+    // let l2GovernanceRelayImplAddress = "";
+    // let l2GovernanceRelayProxyAddress = "";
+    // Optimism Test
+    const l1USXContractAddress = "0xF76eAd4da04BbeB97d29F83e2Ec3a621d0FB3c6e";
+    const l2USXContractAddress = "0xab7020476D814C52629ff2e4cebC7A8cdC04F18E";
+    const l2msdControllerAddress = "0x16E7F5705967B2153473b8411D3faFe257262330";
+    // const l2RouterAddress = "0x5288c571Fd7aD117beA99bF60FE0846C4E84F933";
+    let l2CrossDomainMessengerAddress = "0x4200000000000000000000000000000000000007";
+    let l1USXGatewayProxyAddress = "0x40E862341b2416345F02c41Ac70df08525150dC7";
+    let proxyAdminAddress = "0x5c3712FAa40c1a9C00481bAb0EE7b68C03A7855C";
+    let l2USXGatewayImplAddress = "0x512c242dcBe915ebaD24f9BDcdBBcEFC3dCa2117";
+    let l2USXGatewayProxyAddress = "0xB4d37826b14Cd3CB7257A2A5094507d701fe715f";
+    let l1GovernanceRelayAddress = "0xE5ecB56521B4BF6E38662B2BD85143F5c56cAE90";
+    let l2GovernanceRelayImplAddress = "0x29bdd4E48643Fd23BB6944D7252e5b696f5f50A6";
+    let l2GovernanceRelayProxyAddress = "0x57f76F87CE81638561070389f16D7833080c11a8";
+
+    // 'web3Provider' is a remix global variable object
+    const signer = new ethers.providers.Web3Provider(web3Provider).getSigner();
+
+    // 0. Deploys proxy admin.
+    const proxyAdminName = "ProxyAdmin";
+    const proxyAdminArtifactsPath = `browser/artifacts/contracts/library/${proxyAdminName}.sol/${proxyAdminName}.json`;
+    const proxyAdminMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", proxyAdminArtifactsPath)
+    );
+
+    if (!proxyAdminAddress) {
+      console.log("U r going to deploy proxy admin");
+      // Create an instance of a Contract Factory
+      const proxyAdminFactory = new ethers.ContractFactory(proxyAdminMetadata.abi, proxyAdminMetadata.bytecode, signer);
+      const proxyAdmin = await proxyAdminFactory.deploy();
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await proxyAdmin.deployed();
+      proxyAdminAddress = proxyAdmin.address;
+    }
+    console.log("proxy admin contract address: ", proxyAdminAddress);
+
+    // 1.0 Deploy L2 governance rely implementation contract.
+    const governanceRelayContractName = "L2GovernanceRelay";
+    const governanceRelayPath = `browser/artifacts/contracts/l2/${governanceRelayContractName}.sol/${governanceRelayContractName}.json`;
+    const governanceRelayMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", governanceRelayPath)
+    );
+
+    let l2governanceRelayInitArgs = [
+      l2CrossDomainMessengerAddress,
+      l1GovernanceRelayAddress
+    ];
+    if (!l2GovernanceRelayImplAddress) {
+      console.log("U r going to deploy governance relay implementation contract!");
+      // Create an instance of a Contract Factory
+      const governanceRelayImplFactory = new ethers.ContractFactory(governanceRelayMetadata.abi, governanceRelayMetadata.bytecode, signer);
+      const governanceRelayImpl = await governanceRelayImplFactory.deploy(...l2governanceRelayInitArgs);
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await governanceRelayImpl.deployed();
+      l2GovernanceRelayImplAddress = governanceRelayImpl.address;
+    }
+    console.log("Governance relay implementation contract address: ", l2GovernanceRelayImplAddress);
+    const governanceRelayInIface = new ethers.utils.Interface(governanceRelayMetadata.abi);
+
+    // 1.1 Deploys L1 governance rely proxy contract.
+    const proxyName = "TransparentUpgradeableProxy";
+    const proxyArtifactsPath = `browser/artifacts/@openzeppelin/contracts/proxy/${proxyName}.sol/${proxyName}.json`;
+    const proxyMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", proxyArtifactsPath)
+    );
+
+    if (!l2GovernanceRelayProxyAddress) {
+      console.log("Going to deploy governance relay proxy contract!");
+      const governanceRelayInitData = governanceRelayInIface.encodeFunctionData("initialize", [...l2governanceRelayInitArgs]);
+      console.log("initData is: ", governanceRelayInitData);
+
+      const governanceRelayProxyFactory = new ethers.ContractFactory(proxyMetadata.abi, proxyMetadata.bytecode, signer);
+      const governanceRelayProxy = await governanceRelayProxyFactory.deploy(l2GovernanceRelayImplAddress, proxyAdminAddress, governanceRelayInitData);
+      await governanceRelayProxy.deployed();
+      l2GovernanceRelayProxyAddress = governanceRelayProxy.address;
+    }
+    console.log("L2 governance realy proxy contract address: ", l2GovernanceRelayProxyAddress);
+
+    // 2.0 Deploys L2 USX gateway implementation contract
+    let l2USXGatewayInitArgs = [
+      l2CrossDomainMessengerAddress,
+      l2USXContractAddress,
+      l1USXContractAddress,
+      l1USXGatewayProxyAddress,
+      l2msdControllerAddress
+    ];
+    const l2USXGatewayContractName = "L2USXTokenBridge";
+    const l2USXGatewayPath = `browser/artifacts/contracts/l2/${l2USXGatewayContractName}.sol/${l2USXGatewayContractName}.json`;
+    const l2USXGatewayMetadata = JSON.parse(
+      await remix.call("fileManager", "getFile", l2USXGatewayPath)
+    );
+    if (!l2USXGatewayImplAddress) {
+      console.log("U r going to deploy L2 USX gateway implementation contract!");
+      // Create an instance of a Contract Factory
+      const l2USXGatewayImplFactory = new ethers.ContractFactory(l2USXGatewayMetadata.abi, l2USXGatewayMetadata.bytecode, signer);
+      const l2USXGatewayImpl = await l2USXGatewayImplFactory.deploy(...l2USXGatewayInitArgs);
+      // The contract is NOT deployed yet; we must wait until it is mined
+      await l2USXGatewayImpl.deployed();
+      l2USXGatewayImplAddress = l2USXGatewayImpl.address;
+    }
+    console.log("L2 USX gateway implementation contract address: ", l2USXGatewayImplAddress);
+    const l2USXGatewayInIface = new ethers.utils.Interface(l2USXGatewayMetadata.abi);
+
+    // 2.1 Deploys L2 USX gateway proxy contract
+    if (!l2USXGatewayProxyAddress) {
+      console.log("Going to deploy L2 USX gateway proxy contract!");
+      const l2USXGatewayInitData = l2USXGatewayInIface.encodeFunctionData("initialize", [...l2USXGatewayInitArgs]);
+      console.log("l2 USX Gateway initData is: ", l2USXGatewayInitData);
+
+      const l2USXGatewayProxyFactory = new ethers.ContractFactory(proxyMetadata.abi, proxyMetadata.bytecode, signer);
+      const l2USXGatewayProxy = await l2USXGatewayProxyFactory.deploy(l2USXGatewayImplAddress, proxyAdminAddress, l2USXGatewayInitData);
+      await l2USXGatewayProxy.deployed();
+      l2USXGatewayProxyAddress = l2USXGatewayProxy.address;
+    }
+    console.log("L2 USX Gateway proxy contract address: ", l2USXGatewayProxyAddress);
+
+    console.log("\n");
+    console.log("Please run another script to deploy USX gateway on the L1");
+    console.log("\n");
+
+    console.log("Finish!");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+main()
+  .then(() => console.log('DONE'))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
